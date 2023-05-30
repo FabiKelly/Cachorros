2af35445e8bcec02e7865328e1f65f53e3b27da1
Geovana Cristina Vicente Soares <89870644+Geovanac5643@users.noreply.github.com>
2023-05-30 16:17:14 -0300

    Add files via upload

---
+++ animacao.js
@@ -0,0 +1,33 @@
+// Função para animar o cachorro
+function animateDog() {
+    var dog = document.getElementById("dog");
+    var position = 0;
+    var direction = 1;
+    var maxWidth = document.documentElement.clientWidth - dog.width;
+
+    // Função de animação
+    function frame() {
+        // Atualiza a posição do cachorro
+        position += direction * 10; // Aumenta a quantidade de pixels para mover o cachorro
+
+        // Verifica se o cachorro ultrapassou as bordas da janela
+        if (position >= maxWidth || position <= 0) {
+            // Vira o cachorro
+            dog.style.transform = direction === 1 ? "scaleX(-1)" : "scaleX(1)";
+
+            // Inverte a direção
+            direction *= -1;
+        }
+
+        dog.style.left = position + "px";
+
+        // Chama a próxima animação
+        requestAnimationFrame(frame);
+    }
+
+    // Inicia a animação
+    frame();
+}
+
+// Chama a função de animação quando a página carrega
+window.onload = animateDog;
